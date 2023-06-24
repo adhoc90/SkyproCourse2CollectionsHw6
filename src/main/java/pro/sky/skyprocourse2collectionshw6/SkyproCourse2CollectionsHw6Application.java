35ea21db8716caf8b5b3ea5961a3edfaca3d56e1
leo_r <ad_hoc90@mail.ru>
2023-06-24 21:23:01 +0300

    initial commit

---
+++ src/main/java/pro/sky/skyprocourse2collectionshw6/SkyproCourse2CollectionsHw6Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocourse2collectionshw6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCourse2CollectionsHw6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCourse2CollectionsHw6Application.class, args);
+	}
+
+}
